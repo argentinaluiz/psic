b6f28cb7ae5f42c27f5fc2487a97a289fde2816a
Marisa Endruveit <mendruveit@gmail.com>
2018-05-15 11:13:46 -0300

    Acrescentar nas Factorys as imagens das Pesquisas e dos Recursos

--- database/factories/ResearchFactory.php
+++ database/factories/ResearchFactory.php
@@ -5,6 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\Painel\Research::class, function (Faker $faker) {
     return [
         'title' => $faker->word(10),
+        'image' => 'research.jpg',
         'tag' => $faker->word(10),
         'description' => $faker->sentence(3),
         'year' => rand(2017,2030),
--- database/factories/ToolFactory.php
+++ database/factories/ToolFactory.php
@@ -5,6 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Models\Painel\Tool::class, function (Faker $faker) {
     return [
         'title' => $faker->word(10),
+        'image' => 'research.jpg',
         'description' => $faker->sentence(3),
         'year' => rand(2017,2030),
         'active' => rand(0, 1),
