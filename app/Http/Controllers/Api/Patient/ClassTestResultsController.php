d6d4333287ba820d477c02ec20ecae32a70892e7
Marisa Endruveit <mendruveit@gmail.com>
2018-05-18 22:50:43 -0300

    Mostrar gráfico - resolver o array vazio

--- app/Http/Controllers/Api/Patient/ClassTestResultsController.php
+++ app/Http/Controllers/Api/Patient/ClassTestResultsController.php
@@ -10,6 +10,7 @@ class ClassTestResultsController extends Controller
 {
     public function perSubject(Request $request) //?class_meetings=120
     {
+        $class_meeting_id = $request->query("class_meeting");
         $sumClassTestPoints = "(select sum(questions.point) from questions where questions.class_test_id = class_tests.id)";
         $selects = [
             'patient_class_tests.created_at',
@@ -18,7 +19,7 @@ class ClassTestResultsController extends Controller
        // dd($selects);
         //var_dump($selects);
        // die();
-       //dd(\Auth::user());
+       //dd(\Auth::user()->userable->id);
         //permite que vc crie a query mais próxima do banco de dados
         $results = \DB::table('patient_class_tests')
         ->selectRaw(implode(',', $selects))
@@ -27,12 +28,12 @@ class ClassTestResultsController extends Controller
         //->join('subjects','subjects.id','=','class_meetings.subject_id')
         ->where('patient_id', \Auth::user()->userable->id)
        // ->where('patient_id', 3)
-        ->where('class_tests.class_meeting_id','class_meeting_id')
+        ->where('class_tests.class_meeting_id',$class_meeting_id)
         // ->where('patient_id', 15)
         // ->where('class_tests.class_meeting_id',245)
         ->orderBy('patient_class_tests.created_at', 'asc')
-        ->get();
         //->toSql();
+        ->get();
         $results->map(function ($item) { //\stdClass
             $item->created_at = (new Carbon($item->created_at))->format(Carbon::ISO8601);
             return $item;
