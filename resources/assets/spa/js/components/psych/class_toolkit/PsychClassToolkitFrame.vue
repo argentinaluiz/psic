1ff974f4f9e3a3786c9445443f87d192145fe08b
Marisa Endruveit <mendruveit@gmail.com>
2018-07-21 12:00:54 -0300

    Alterações nos componentes de acordo com a nova serialização

--- resources/assets/spa/js/components/psych/class_toolkit/PsychClassToolkitFrame.vue
+++ resources/assets/spa/js/components/psych/class_toolkit/PsychClassToolkitFrame.vue
@@ -4,12 +4,12 @@
             <span class="corner"></span>
 
             <div class="image">
-                <img class="img-responsive" :src="`/storage/tool/${classToolkit.subRanks.tools.image}`" alt=""> 
+                <img class="img-responsive" :src="`/storage/tool/${classToolkit.subRanks.subSubRanks.tools.image}`" alt=""> 
             </div>
             <div class="file-name">
-                <h5>{{ classToolkit.subRanks.tools.title }}</h5>
-                <p class="cut">{{ classToolkit.subRanks.tools.description }}</p>
-                <small>{{ classToolkit.subRanks.tools.year }}</small>
+                <h5>{{ classToolkit.subRanks.subSubRanks.tools.title }}</h5>
+                <p class="cut">{{ classToolkit.subRanks.subSubRanks.tools.description }}</p>
+                <small>{{ classToolkit.subRanks.subSubRanks.tools.year }}</small>
             </div>
         </a>
     </div>
--- resources/assets/spa/js/components/psych/class_toolkit/PsychClassToolkitSubRankList.vue
+++ resources/assets/spa/js/components/psych/class_toolkit/PsychClassToolkitSubRankList.vue
@@ -20,12 +20,12 @@
                                         <a href="#">
                                             <span class="corner"></span>
                                             <div class="image">
-                                                <img class="img-responsive" :src="`/storage/tool/${classToolkit.subRanks.tools.image}`" alt="">
+                                                <img class="img-responsive" :src="`/storage/tool/${classToolkit.subRanks.subSubRanks.tools.image}`" alt="">
                                             </div>
                                             <div class="file-name">
-                                                <h5>{{ classToolkit.subRanks.tools.title }}</h5>
-                                                <p class="cut">{{ classToolkit.subRanks.tools.description }}</p>
-                                                <small>{{ classToolkit.subRanks.tools.year }}</small>
+                                                <h5>{{ classToolkit.subRanks.subSubRanks.tools.title }}</h5>
+                                                <p class="cut">{{ classToolkit.subRanks.subSubRanks.tools.description }}</p>
+                                                <small>{{ classToolkit.subRanks.subSubRanks.tools.year }}</small>
                                             </div>
                                         </a>
                                     </div>
@@ -40,12 +40,12 @@
                             <span class="corner"></span>
 
                             <div class="image">
-                                <img class="img-responsive" :src="`/storage/tool/${classToolkit.subRanks.tools.image}`" alt="">
+                                <img class="img-responsive" :src="`/storage/tool/${classToolkit.subRanks.subSubRanks.tools.image}`" alt="">
                             </div>
                             <div class="file-name">
-                                <h5>{{ classToolkit.subRanks.tools.title }}</h5>
-                                <p class="cut">{{ classToolkit.subRanks.tools.description }}</p>
-                                <small>{{ classToolkit.subRanks.tools.year }}</small>
+                                <h5>{{ classToolkit.subRanks.subSubRanks.tools.title }}</h5>
+                                <p class="cut">{{ classToolkit.subRanks.subSubRanks.tools.description }}</p>
+                                <small>{{ classToolkit.subRanks.subSubRanks.tools.year }}</small>
                             </div>
                         </a>
                     </div>
