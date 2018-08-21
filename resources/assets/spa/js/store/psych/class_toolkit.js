bfbcf5755a3454de1bad7f48204f35d87dba3530
Marisa Endruveit <mendruveit@gmail.com>
2018-08-21 17:57:04 -0300

    Acrescentar mais um nível de repetição dos dados para a API

--- resources/assets/spa/js/store/psych/class_toolkit.js
+++ resources/assets/spa/js/store/psych/class_toolkit.js
@@ -18,7 +18,7 @@ const actions = {
     query(context){
         Psychoanalyst.classToolkit.query()
             .then(response => {
-                context.commit('setClassToolkits', response.data.data);
+                context.commit('setClassToolkits', response.data.data.data);
             });
     },
     get(context, classToolkitId){
