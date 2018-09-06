6ad1c2fc0252eacfa58bf3845c0791da537a189b
Marisa Endruveit <mendruveit@gmail.com>
2018-09-06 14:56:52 -0300

    Administrativo: Adicionar campos no form do SubRank

--- app/Forms/SubRankForm.php
+++ app/Forms/SubRankForm.php
@@ -13,6 +13,22 @@ class SubRankForm extends Form
         ->add('name', 'text', [
             'label' => 'Nome',
             'rules' => 'required|max:255'
+        ])
+
+        ->add('rank_id', 'entity', [
+                'label' => 'Categoria', 
+                'class' => '\App\Models\Painel\Rank', // Entity that holds data
+                'property' => 'name', // Value that will be used as a label for each choice option, default: name 
+                'property_key' => 'id',
+                'selected' => $this->model ? $this->model->SubRank->rank_id : '5'
+        ])
+
+        ->add('parent_id', 'entity', [
+                'label' => 'Sub SubCategoria (parent)', 
+                'class' => '\App\Models\Painel\SubRank', 
+                'property' => 'name', 
+                'property_key' => 'id',
+                'selected' => $this->model ? $this->model->SubRank->parent_id : '35',
         ]);
     }
 }
